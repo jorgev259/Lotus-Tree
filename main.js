042376ce4b66d51add098a686c76b4122fdeecf1
jorgev259 <jorgev259@outlook.es>
2019-10-07 00:01:20 -0500

    standarizing

--- main.js
+++ main.js
@@ -17,14 +17,14 @@
   const glob = require('glob')
 
   await fs.remove('./pnpm-lock.yaml')
-  let modulesObjectList = require('./data/modules.json')
+  const modulesObjectList = require('./data/modules.json')
   const startBot = require('./bot.js')
 
-  let remoteLotus = (await gitModule('ls-remote')).split('\n').filter(e => !e.startsWith('From'))[0].split('\t')[0]
-  let localLotus = (await gitModule('rev-parse HEAD')).split('\n')[0]
+  const remoteLotus = (await gitModule('ls-remote')).split('\n').filter(e => !e.startsWith('From'))[0].split('\t')[0]
+  const localLotus = (await gitModule('rev-parse HEAD')).split('\n')[0]
 
   if (remoteLotus !== localLotus) {
-    console.log(`Updating LotusTree`)
+    console.log('Updating LotusTree')
     try {
       await gitModule('git pull')
     } catch (err) {
@@ -32,11 +32,11 @@
       console.log('LotusTree update failed. Use \'git pull\' for more detailed information')
       process.exit(0)
     }
-    console.log(`Updated LotusTree. Restart the app.`)
+    console.log('Updated LotusTree. Restart the app.')
     process.exit(0)
   }
 
-  console.log(`LotusTree is up to date.`)
+  console.log('LotusTree is up to date.')
 
   fs.removeSync('modules_old')
   fs.removeSync('package.json')
@@ -49,15 +49,15 @@
   await Promise.all(modulesObjectList.map(moduleObject => {
     return new Promise(async (resolve, reject) => {
       switch (moduleObject.type) {
-        case 'local':
-          let files = glob.sync(`${moduleObject.path}**`, { nodir: true })
+        case 'local': {
+          const files = glob.sync(`${moduleObject.path}**`, { nodir: true })
             .map(e => { return { original: e, path: e.replace(moduleObject.path, '') } })
             .filter(e => !e.path.startsWith('node_modules') && (!moduleObject.modules || moduleObject.modules.includes(e.path.split('/')[1])))
 
-          let promises = []
+          const promises = []
           files.forEach(file => {
             if (file.path.endsWith('dependencies.json')) {
-              let moduleName = file.path.split(`modules/`)[1].split('/')[0]
+              const moduleName = file.path.split('modules/')[1].split('/')[0]
               promises.push(fs.copySync(file.original, `data/${moduleName}_dependencies.json`))
             } else if (file.path.endsWith('.json')) {
               promises.push(fs.copySync(file.original, file.path.replace('modules/', 'data/')))
@@ -69,13 +69,14 @@
           resolve()
 
           break
+        }
 
-        case 'git':
+        case 'git': {
           if (!(await fs.pathExists(`./repos/${moduleObject.name}/.git`))) {
             await git(`clone ${moduleObject.url} ${moduleObject.name}`)
           } else {
-            let remote = (await git('ls-remote', moduleObject.name)).split('\n').filter(e => !e.startsWith('From'))[0].split('\t')[0]
-            let local = (await git('rev-parse HEAD', moduleObject.name)).split('\n')[0]
+            const remote = (await git('ls-remote', moduleObject.name)).split('\n').filter(e => !e.startsWith('From'))[0].split('\t')[0]
+            const local = (await git('rev-parse HEAD', moduleObject.name)).split('\n')[0]
 
             if (remote !== local) {
               console.log(`Updating repository ${moduleObject.name}`)
@@ -86,13 +87,13 @@
             }
           }
 
-          let promises2 = []
-          let fileList = glob.sync(`repos/${moduleObject.name}/**`, { nodir: true })
+          const promises2 = []
+          const fileList = glob.sync(`repos/${moduleObject.name}/**`, { nodir: true })
             .filter(e => !moduleObject.modules || moduleObject.modules.includes(e.split('/')[3]))
 
           fileList.forEach(file => {
             if (file.endsWith('dependencies.json')) {
-              let moduleName = file.split(`repos/${moduleObject.name}/modules/`)[1].split('/')[0]
+              const moduleName = file.split(`repos/${moduleObject.name}/modules/`)[1].split('/')[0]
               promises2.push(fs.copySync(file, `data/${moduleName}_dependencies.json`))
             } else if (file.endsWith('.json')) {
               promises2.push(fs.copySync(file, file.replace(`repos/${moduleObject.name}/modules/`, 'data/')))
@@ -103,6 +104,7 @@
           resolve()
 
           break
+        }
       }
     })
   }))
@@ -110,12 +112,12 @@
   fs.copySync('modules_new', 'modules')
   fs.removeSync('modules_new')
   fs.copySync('modules_basic', 'modules')
-  let packageJSON = require('./package.json')
+  const packageJSON = require('./package.json')
 
-  let fileList = glob.sync(`data/**_dependencies.json`, { nodir: true })
+  const fileList = glob.sync('data/**_dependencies.json', { nodir: true })
   let change = false
   fileList.forEach(file => {
-    let deps = require(`./${file}`)
+    const deps = require(`./${file}`)
     Object.keys(deps).forEach(depKey => {
       if (!packageJSON.dependencies[depKey] || packageJSON.dependencies[depKey] !== deps[depKey]) {
         change = true
@@ -126,7 +128,7 @@
 
   if (change) {
     fs.outputFileSync('package.json', JSON.stringify(packageJSON, null, 4))
-    console.log(`Dependencies updated. Run "npm i" to install them`)
+    console.log('Dependencies updated. Run "npm i" to install them')
     // process.exit(0)
   }
 
